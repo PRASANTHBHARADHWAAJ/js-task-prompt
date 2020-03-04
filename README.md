e8f115b2d11de6f007abf54f0466f4a1bdb1714b
PRASANTHBHARADHWAAJ <prasanth99waaj@gmail.com>
2020-03-04 16:24:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-pa6y5p
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-pa6y5p)
---
+++ index.js
@@ -0,0 +1,4 @@
+let a=prompt("enter the name")
+let b=prompt("start slice")
+let c=prompt("stop slice")
+alert(`${a.slice(b,c)}`)
