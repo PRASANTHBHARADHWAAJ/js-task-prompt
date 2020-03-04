470f24ae02db9bb6c884157d26547c25922e712c
PRASANTHBHARADHWAAJ <prasanth99waaj@gmail.com>
2020-03-04 17:25:46 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-let a=prompt("enter the name")
-let b=prompt("start slice")
-let c=prompt("stop slice")
-alert(`${a.slice(b,c)}`)
+// let a=prompt("enter the name")
+// let b=prompt("start slice")
+// let c=prompt("stop")
+// let a1=parseInt(b)
+// let a2=parseInt(c)
+// alert(typeof a1)
+// alert(typeof a2)
+// ((typeof a1) != number || (typeof a2) != number)? alert("please enter valid number"): alert(`${a.slice(b,c)}`);
